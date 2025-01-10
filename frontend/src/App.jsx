a65684af4c8d2897828369d43754070f627b455f
prathamkumar <prathamkumar1985@gmail.com>
2025-01-11 02:13:51 +0530

    7-Create-UI-files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/home/HomePage";
 import SignupPage from "./pages/auth/SignupPage";
+import Sidebar from "./components/common/Sidebar";
 import LoginPage from "./pages/auth/LoginPage";
 import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignupPage />} />
